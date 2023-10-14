d8ae24b4681d37dbe18357f33e481b2b83a575a8
Mykyta Terpelovskyi <81818898+Dremorio@users.noreply.github.com>
2023-10-14 14:04:41 +0300

    add Email.py with validation

---
+++ personal_assistant_folder/personal_assistant_folder/Email.py
@@ -0,0 +1,20 @@
+from Field import Field
+import re
+
+
+class Email(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate()
+
+    def validate(self):
+        if self._value:
+            email_re = re.compile(
+                r"^(?![.@])[a-zA-Z0-9._-]+(?<!\.)@(?![.])(?!\.)[a-zA-Z0-9.-]+(?<![.])\.[a-zA-Z]{2,6}$"
+            )
+
+            if len(self._value) > 20:
+                raise ValueError('Email is too long.')
+
+            if not email_re.match(self._value):
+                raise ValueError('Invalid email format.')
