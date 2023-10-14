b1cf98b7b0fdbf29948a19da19ae0ce939ea1b7a
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-10-14 21:44:56 +0300

    Update Birthday.py

--- personal_assistant_folder/personal_assistant_folder/Birthday.py
+++ personal_assistant_folder/personal_assistant_folder/Birthday.py
@@ -1,31 +1,31 @@
 from datetime import datetime
-from Field import Field
 
-class Birthday(Field):
-    def validate(self):
-        self._check_type()
-        self._check_future_date()
-        self._check_birth_year()
+class Birthday:
+    def __init__(self, value):
+        self.value = value
 
-    def _check_type(self):
-        if not isinstance(self.value, datetime):
-            raise ValueError("Birthday must be a datetime object")
-
-    def _check_future_date(self):
-        now = datetime.now()
-        if self.value > now:
-            raise ValueError("Birthday cannot be in the future")
-
-    def _check_birth_year(self):
-        if self.value.year < 1900:
-            raise ValueError("Year of birth cannot be earlier than 1900")
+    def _check_valid_date(self, date):
+        try:
+            datetime.strptime(date, '%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
 
     def _normalize_year(self):
-        now = datetime.now()
-        return self.value.replace(year=now.year)
+        today = datetime.now()
+        return self.value.replace(year=today.year)
 
-    def validate_and_normalize(self):
-        self._check_type()
-        self._check_future_date()
-        self._check_birth_year()
-        return self._normalize_year()
+    def validate(self):
+        if not self._check_valid_date(self.value.strftime('%Y-%m-%d')):
+            raise ValueError("Invalid date format for birthday")
+        self.value = self._normalize_year()
+
+    def days_until_birthday(self):
+        today = datetime.now()
+        next_birthday = self.value.replace(year=today.year)
+
+        if today > next_birthday:
+            next_birthday = next_birthday.replace(year=today.year + 1)
+
+        days_until_birthday = (next_birthday - today).days
+        return days_until_birthday
