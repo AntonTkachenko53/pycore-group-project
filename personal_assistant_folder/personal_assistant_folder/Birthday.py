498d0b75dc0802e3258c7d6ba8db620db9369afc
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-10-14 23:13:27 +0300

    Update Birthday.py

--- personal_assistant_folder/personal_assistant_folder/Birthday.py
+++ personal_assistant_folder/personal_assistant_folder/Birthday.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from Field import Field
 
 class Birthday(Field):
+    DATE_FORMAT = '%d.%m.%Y'
+
     def __init__(self, value):
         super().__init__(value)
         self.validate()
