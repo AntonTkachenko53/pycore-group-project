c99135bada02db41d690a9b2f6dbef832848adaa
AntonTkachenko53 <gogochristofour@gmail.com>
2023-10-15 16:38:39 +0300

    small Name.py changes
    changed magic number for MIN_LENGTH

--- personal_assistant_folder/personal_assistant_folder/Name.py
+++ personal_assistant_folder/personal_assistant_folder/Name.py
@@ -2,11 +2,13 @@ from Field import Field
 
 
 class Name(Field):
+    MIN_LENGTH = 3
+
     def __init__(self, value):
         super().__init__(value)
         self.validate()
 
     def validate(self):
         check_name = self._value.strip()
-        if len(check_name) < 3 or check_name.isdigit():
+        if len(check_name) < self.MIN_LENGTH or check_name.isdigit():
             raise ValueError("Invalid name. Name should be 3+ chars length and don`t consist only with numbers")
