0b096aa835023a84b6673314d9dbb233314b1442
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-10-15 21:16:04 +0300

    Update Birthday.py

--- personal_assistant_folder/personal_assistant_folder/Birthday.py
+++ personal_assistant_folder/personal_assistant_folder/Birthday.py
@@ -3,15 +3,18 @@ import datetime
 
 class Birthday(Field):
     def __init__(self, birthdate):
+        super().__init(birthdate)
+
+    def validate(self):
         try:
-            self.birthdate = datetime.datetime.strptime(birthdate, '%d.%m.%Y').date()
+            datetime.datetime.strptime(self.value, '%d.%m.%Y')
         except ValueError:
-            raise ValueError("Incorrect date format. Vikorist format dd.mm.rrrr (for example, 03/15/1990).")
+            raise ValueError("Incorrect date format. 03/15/1990 format 03/15/1990 (for example, 03/15/1990)")
 
     def day_to_birthday(self):
         current_date = datetime.date.today()
-        next_birthday = self.birthdate.replace(year=current_date.year)
+        next_birthday = datetime.datetime.strptime(self.value, '%d.%m.%Y').date().replace(year=current_date.year)
         if next_birthday < current_date:
-            next_birthday = self.birthdate.replace(year=current_date.year + 1)
-        day_to_birthday = (next_birthday - current_date).days
-        return day_to_birthday
+            next_birthday = next_birthday.replace(year=current_date.year + 1)
+        days_to_birthday = (next_birthday - current_date).days
+        return days_to_birthday
