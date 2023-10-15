81eca650642cc692f35289f413afc04e52ee8cf2
s.kravtsov <s.kravtsov@hotmail.com>
2023-10-15 17:41:51 +0300

    Methods for work with Tags list in Note

--- personal_assistant_folder/personal_assistant_folder/Note.py
+++ personal_assistant_folder/personal_assistant_folder/Note.py
@@ -1,5 +1,6 @@
 from notes.Title import Title
 from notes.Content import Content
+from notes.Tag import Tag
 
 
 class Note:
@@ -7,3 +8,26 @@ class Note:
         self.title = Title(title)
         self.content = Content(content)
         self.tags = set()
+
+    def add_tag(self, value):
+        """
+        Add new tag in tags list for note
+        :param value: new tag
+        """
+        self.tags.add(Tag(value))
+
+    def check_tag(self, value):
+        """
+        Check in note has this tag
+        :param value: it`s tag which need check
+        :return: True if it has. False if not
+        """
+        return True if Tag(value) in self.tags else False
+
+    def delete_tag(self, value):
+        """
+        Delete tag from tags list for note
+        :param value: tag
+        """
+        if self.check_tag(value):
+            self.tags.discard(Tag(value))
