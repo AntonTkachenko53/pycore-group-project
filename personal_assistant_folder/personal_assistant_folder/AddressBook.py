f0ff1ceb5434e69600b0037d3fa9a6a0a6c3cf44
Mykyta Terpelovskyi <81818898+Dremorio@users.noreply.github.com>
2023-10-15 22:26:09 +0300

    Update AddressBook.py, added iterator and serialization

--- personal_assistant_folder/personal_assistant_folder/AddressBook.py
+++ personal_assistant_folder/personal_assistant_folder/AddressBook.py
@@ -1,5 +1,6 @@
 from collections import UserDict
 from Record import Record
+import pickle
 
 
 class AddressBook(UserDict):
@@ -7,10 +8,10 @@ class AddressBook(UserDict):
     MAX_LENGTH_ADD = 5
 
     def add_record(self, user_input):
-        '''
+        """
         тут введений нпут може містити тільки значення, від двох до п'яти, котрі записуються в record,
         порядок наступний: name, phone, далі ключові аргументи, котрі за замовчуванням = None: birthday, email, address.
-        '''
+        """
         commands = user_input.strip().split(' ')
         if not (self.MIN_LENGTH_ADD <= len(commands) <= self.MAX_LENGTH_ADD):
             raise ValueError('Enter correct info to add a record')
@@ -24,3 +25,19 @@ class AddressBook(UserDict):
 
     def find_record(self, searching_str: str):
         pass
+
+    def iterator(self, num_of_records=5):
+        records = list(self.data.values())
+        for i in range(0, len(records), num_of_records):
+            yield records[i:i + num_of_records]
+
+    def save_to_file(self, filename='address_book.bin'):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.data, file)
+
+    def load_from_file(self, filename='address_book.bin'):
+        try:
+            with open(filename, 'rb') as file:
+                self.data = pickle.load(file)
+        except FileNotFoundError:
+            print(f'File {filename} not found')
