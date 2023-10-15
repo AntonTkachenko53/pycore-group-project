276759d75242d3c4c37027573a1817e3c1117977
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-10-15 22:22:39 +0300

    Update Birthday.py

--- personal_assistant_folder/personal_assistant_folder/Birthday.py
+++ personal_assistant_folder/personal_assistant_folder/Birthday.py
@@ -3,17 +3,18 @@ import datetime
 
 class Birthday(Field):
     def __init__(self, birthdate):
-        super().__init(birthdate)
+        super().__init__(birthdate)
+        self.validate()
 
     def validate(self):
         try:
-            datetime.datetime.strptime(self.value, '%d.%m.%Y')
+            datetime.datetime.strptime(self._value, '%d.%m.%Y')
         except ValueError:
-            raise ValueError("Incorrect date format. 03/15/1990 format 03/15/1990 (for example, 03/15/1990)")
+            raise ValueError("correct format is 03.10.1990")
 
     def day_to_birthday(self):
         current_date = datetime.date.today()
-        next_birthday = datetime.datetime.strptime(self.value, '%d.%m.%Y').date().replace(year=current_date.year)
+        next_birthday = datetime.datetime.strptime(self._value, '%d.%m.%Y').date().replace(year=current_date.year)
         if next_birthday < current_date:
             next_birthday = next_birthday.replace(year=current_date.year + 1)
         days_to_birthday = (next_birthday - current_date).days
