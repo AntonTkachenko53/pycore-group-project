66cf13680dfa8501743d7d67a823a186b5968db5
AntonTkachenko53 <gogochristofour@gmail.com>
2023-10-15 17:35:43 +0300

    Update add_record method

--- personal_assistant_folder/personal_assistant_folder/AddressBook.py
+++ personal_assistant_folder/personal_assistant_folder/AddressBook.py
@@ -4,8 +4,23 @@ from Record import Record
 
 class AddressBook(UserDict):
 
-    def add_record(self, user_input):  # Інпут може місти від двох до п'яти значень, котрі записуються в record
-        commands = user_input.strip().split(', ')
+    def add_record(self, user_input):
+        '''
+        тут введений нпут може містити тільки значення, від двох до п'яти, котрі записуються в record,
+        порядок наступний: name, phone, далі ключові аргументи, котрі за замовчуванням = None: birthday, email, address.
+        '''
+        commands = user_input.strip().split(' ')
         min_length = 2
-        max_length = 
-        if len(commands)
+        max_length = 5
+        if not (min_length <= len(commands) <= max_length):
+            raise ValueError('Enter correct info to add a record')
+
+        name, phone, *args = commands  # Розпакування перших двох значень та всіх інших у змінну args
+
+        try:
+            self.data[name] = Record(name, phone, *args)
+        except ValueError:
+            pass
+
+    def find_record(self, searching_str: str):
+        pass
