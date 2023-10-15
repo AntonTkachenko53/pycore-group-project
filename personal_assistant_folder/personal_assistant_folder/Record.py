e5f4382d848cdf0ad108c5ffb8fbfa69628d7161
Symchyra-M <137912223+Symchyra-M@users.noreply.github.com>
2023-10-15 22:29:00 +0200

    add metods for Record
    add ( add_edit, remove) metods for Record

--- personal_assistant_folder/personal_assistant_folder/Record.py
+++ personal_assistant_folder/personal_assistant_folder/Record.py
@@ -7,16 +7,42 @@ from Birthday import Birthday
 class Record:
     def __init__(self, name, phone, birthday=None, email=None, address=None):
         self.name = Name(name)
-        self.phones = Phone(phone)
+        self.phone = Phone(phone)
         self.birthday = Birthday(birthday) if birthday else None
         self.email = Email(email) if email else None
         self.address = Address(address) if address else None
 
-    def add(self):
-        pass
+    def add_edit(self, info, value):
+        if info == "phone":
+            try:
+                self.phone = Phone(value)
+            except ValueError:
+                pass
+        elif info == "email":
+            try:
+                self.email = Email(value)
+            except ValueError:
+                pass
+        elif info == "name":
+            try:
+                self.name = Name(value)
+            except ValueError:
+                pass
+        elif info == "birthday":
+            try:
+                self.birthday = Birthday(value)
+            except ValueError:
+                pass
+        elif info == "address":
+            try:
+                self.address = Address(value)
+            except ValueError:
+                pass
 
-    def remove(self):
-        pass
-
-    def edit(self):
-        pass
+    def remove(self, info):
+        if info == "email":
+            self.email = None
+        elif info == "address":
+            self.address = None
+        elif info == "birthday":
+            self.address = None
