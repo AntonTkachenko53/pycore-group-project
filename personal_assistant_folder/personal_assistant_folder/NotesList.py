7c020f9ebde29564c370ebfad7bf234fbeb2a874
Symchyra-M <137912223+Symchyra-M@users.noreply.github.com>
2023-10-16 19:59:56 +0200

    Update NoteList.py
    change logiс in  edit_note metod

--- personal_assistant_folder/personal_assistant_folder/NotesList.py
+++ personal_assistant_folder/personal_assistant_folder/NotesList.py
@@ -32,29 +32,31 @@ class NotesList:
             raise ValueError("Invalid data format\nExample: Title;Content;#tag1,#tag2,#tag3")
 
     def edit_note(self, title, new_value):
-        # Find the note by title
-        for note in self.noteslist:
-            if str(note.title) == title:
-                # Split the new value into title, content, and tags
-                try:
-                    new_title, new_content, new_tags = new_value.split(';')
-                except ValueError:
-                    raise ValueError("Invalid data format for editing\nExample: Title;Content;#tag1,#tag2,#tag3")
+        # Search for a note by title
+        note_to_edit = next((note for note in self.noteslist if note.title == title), None)
 
-                # Update the note's title and content
-                note.title = new_title
-                note.content = new_content
+        if note_to_edit:
+            # Split the new value into title, content, and tags using the delimiter ";"
+            try:
+                new_title, new_content, new_tags = new_value.split(';')
+            except ValueError:
+                raise ValueError("Invalid data format for editing\nExample: Title;Content;#tag1,#tag2,#tag3")
 
-                # Update the tags
-                note.tags.clear()
-                if new_tags:
-                    tags_list = new_tags.split(',')
-                    for tag in tags_list:
-                        note.add_tag(tag)
-                return
+            # Update the title and content of the note
+            note_to_edit.title = new_title
+            note_to_edit.content = new_content
 
-        # If the note with the given title was not found
-        raise ValueError(f"Note with title '{title}' not found")
+            # Clear the list of note tags
+            note_to_edit.tags.clear()
+
+            # If there are new tags, split them by comma and add them to the note
+            if new_tags:
+                tags_list = new_tags.split(',')
+                for tag in tags_list:
+                    note_to_edit.add_tag(tag)
+        else:
+            # If a note with the specified title is not found, raise an exception
+            raise ValueError(f"Note with title '{title}' not found")
 
     def delete(self, value):
         # !!!Attention!!!
