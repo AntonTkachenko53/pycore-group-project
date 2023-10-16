0f39fa45667d23d3c54b8874974d905f6f7e5cab
s.kravtsov <s.kravtsov@hotmail.com>
2023-10-17 00:37:28 +0300

    first work version with classes methods

--- personal_assistant_folder/personal_assistant_folder/personal_assistant.py
+++ personal_assistant_folder/personal_assistant_folder/personal_assistant.py
@@ -2,246 +2,388 @@ import os
 from sorting_files.Sorting import Sorting
 from AddressBook import AddressBook
 from NotesList import NotesList
+from Serialization import Serialization
+from Pagination import Paginator
 
 """
 PythonCore 17 group №6 personal assistant project
 """
 
-class ConsoleManager:
-    @staticmethod
-    def clear_console():
-        if os.name == 'nt':  # Windows
-            os.system('cls')
-        else:  # MacOS и Linux
-            os.system('clear')
+FILE_CONTACTS = 'contacts.bin'
+FILE_NOTES = 'notes.bin'
+
+menu_main = [
+    "*** Menu ***",
+    "1. AddressBook",
+    "2. Notes",
+    "3. Sorting Files",
+    "4. Exit"
+]
+menu_addressbook = [
+    "*** AddressBook Menu ***",
+    "1. Find",
+    "2. Add",
+    "3. Edit",
+    "4. Delete",
+    "5. Show All",
+    "6. Exit"
+]
+menu_single_contact = [
+    "*** Edit Contact Menu ***",
+    "1. Name",
+    "2. Phone",
+    "3. Email",
+    "4. Address",
+    "5. Birthday",
+    "6. Edit all contact",
+    "7. Exit"
+]
+menu_noteslist = [
+    "*** NotesList Menu ***",
+    "1. Find",
+    "2. Add",
+    "3. Edit",
+    "4. Delete",
+    "5. Show All",
+    "6. Exit"
+]
+menu_single_note = [
+    "*** Edit Contact Menu ***",
+    "1. Title",
+    "2. Content",
+    "3. Tags",
+    "4. Edit all",
+    "5. Exit"
+]
+
+
+# addressbook = AddressBook(FILE_CONTACTS)
+# noteslist = NotesList(FILE_NOTES)
+
+# Очистка консоли при переходе между разделами меню
+def clear_console():
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # MacOS и Linux
+        os.system('clear')
 
 
 def invalid_type():
-    ConsoleManager.clear_console()
+    clear_console()
     print("Invalid choice. try again.")
 
-menu_main = ["*** Menu ***", "1. AddressBook", "2. Notes", "3. Sorting Files", "4. Exit"]
-menu_addressbook = ["*** AddressBook Menu ***", "1. Find", "2. Add", "3. Edit", "4. Delete", "5. Show All", "6. Exit"]
-menu_single_contact = ["*** Edit Contact Menu ***", "1. Name", "2. Phone", "3. Email", "4. Address", "5. Birthday", "6. Edit all contact", "7. Exit"]
-menu_noteslist = ["*** NotesList Menu ***", "1. Find", "2. Add", "3. Edit", "4. Delete", "5. Show All", "6. Exit"]
-menu_single_note = ["*** Edit Contact Menu ***", "1. Title", "2. Content", "3. Tags", "4. Edit all", "5. Exit"]
 
-def print_menu_items(list):
+def wait_to_continue():
+    print()
+    input('Press enter to continue')
+
+
+def print_menu_items(list: list) -> None:
+    print("------------------")
     for item in list:
         print(item)
-
+    print("------------------")
+    print()
 
 
 while True:
-    ConsoleManager.clear_console()
-    print("------------------")
+    clear_console()
     print_menu_items(menu_main)
-    print("------------------")
 
     choice1 = input("Where are you want to start: ")
-
+    # AddressBook Menu
     if choice1 == "1":
-        ConsoleManager.clear_console()
+        clear_console()
         while True:
-            ConsoleManager.clear_console()
-            print("------------------")
+            clear_console()
             print_menu_items(menu_addressbook)
-            print("------------------")
 
             choice2 = input("Choose an item: ")
-
+            # Find Contact
             if choice2 == "1":
-                ConsoleManager.clear_console()
-                command = input("Enter info: ")
-                print(f"Contact with info: {command}")
-                print()
-                input('Press enter to continue')
+                clear_console()
+
+                try:
+                    command = input("Enter info: ")
+                    contacts = addressbook.find_record(command)
+                    if contacts:
+                        for contact in contacts:
+                            print(contact)
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
+            # Add contact
             elif choice2 == "2":
-                ConsoleManager.clear_console()
-                print('Add Contact.')
+                clear_console()
                 print('Example: name phone email address birthday')
-                command = input("Enter info: ")
-                print(f"You wrote: {command}")
-                print()
-                input('Press enter to continue')
+
+                try:
+                    command = input("Enter info: ")
+                    addressbook.add_record(command)
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
+
+            # Edit contact
             elif choice2 == "3":
-                ConsoleManager.clear_console()
 
                 while True:
-                    ConsoleManager.clear_console()
-                    print("------------------")
+                    clear_console()
                     print_menu_items(menu_single_contact)
-                    print("------------------")
 
                     choice3 = input("Choose an item: ")
 
                     if choice3 == "1":
-                        ConsoleManager.clear_console()
-                        command = input("Enter name: ")
-                        print(f"New name: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            command = input("Enter name: ")
+                            print(f"New name: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "2":
-                        ConsoleManager.clear_console()
-                        command = input("Enter phone: ")
-                        print(f"New phone: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            command = input("Enter phone: ")
+                            print(f"New phone: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "3":
-                        ConsoleManager.clear_console()
-                        command = input("Enter email: ")
-                        print(f"New email: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            command = input("Enter email: ")
+                            print(f"New email: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "4":
-                        ConsoleManager.clear_console()
-                        command = input("Enter address: ")
-                        print(f"New address: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            command = input("Enter address: ")
+                            print(f"New address: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "5":
-                        ConsoleManager.clear_console()
-                        command = input("Enter birthday: ")
-                        print(f"New birthday: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            command = input("Enter birthday: ")
+                            print(f"New birthday: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "6":
-                        ConsoleManager.clear_console()
-                        print('Add Contact.')
-                        print('Example: name phone email address birthday')
-                        command = input("Enter info: ")
-                        print(f"You wrote: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+
+                        try:
+                            print('Example: name phone email address birthday')
+                            command = input("Enter info: ")
+                            print(f"You wrote: {command}")
+                        except ValueError as e:
+                            print(e)
+
+                        wait_to_continue()
                     elif choice3 == "7":
-                        ConsoleManager.clear_console()
+                        clear_console()
                         break
                     else:
                         invalid_type()
-
+            # Delete contact
             elif choice2 == "4":
-                ConsoleManager.clear_console()
-                command = input("Delete contact: ")
-                print(f"{command} deleted")
-                print()
-                input('Press enter to continue')
+                clear_console()
+
+                try:
+                    command = input("Delete (contact name): ")
+                    addressbook.delete_record(command)
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
+            # Show all
             elif choice2 == "5":
-                ConsoleManager.clear_console()
-                print(f"Contacts List")
-                print()
-                input('Press enter to continue')
+                clear_console()
+
+                try:
+                    notes_paginator = Paginator(addressbook)
+                    for page in notes_paginator:
+                        for record in page:
+                            print(record)
+                        input('Press Enter to show the next page...')
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
             elif choice2 == "6":
-                ConsoleManager.clear_console()
+                clear_console()
                 break
             else:
                 invalid_type()
 
+    # Notes Menu
     elif choice1 == "2":
-        ConsoleManager.clear_console()
         while True:
-            ConsoleManager.clear_console()
-            print("------------------")
+            clear_console()
             print_menu_items(menu_noteslist)
-            print("------------------")
 
             choice2 = input("Choice item: ")
-
+            # Find Note(s)
             if choice2 == "1":
-                ConsoleManager.clear_console()
-                command = input("Enter Title: ")
-                print(f"Note with title: {command}")
-                print()
-                input('Press enter to continue')
-            elif choice2 == "2":
-                ConsoleManager.clear_console()
-                print('Edit whole Note')
-                print('Example: title;content;#tag1,#tag2')
-                command = input("Enter info: ")
-                print(f"You wrote: {command}")
-                print()
-                input('Press enter to continue')
-            elif choice2 == "3":
-                ConsoleManager.clear_console()
+                clear_console()
+                try:
+                    command = input("Find note(s): ")
+                    # TODO: Доработать когда обновлюсь с MAIN
+                    # if command.startswith('#'):
+                    #     notes = noteslist.find_sort(command)
+                    # else:
+                    # TODO: next line move right after uncomment
+                    notes = noteslist.find_note(command)
 
+                    if notes:
+                        for note in notes:
+                            print(note)
+
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
+            # Add Note
+            elif choice2 == "2":
+                clear_console()
+                print('Example: title;content;#tag1,#tag2')
+
+                try:
+                    command = input("Enter info: ")
+                    note = noteslist.add(command)
+                except ValueError as e:
+                    print(e)
+
+                print(f"Note was saved")
+                wait_to_continue()
+
+            # Edit Note
+            elif choice2 == "3":
+                # TODO: пересмотреть механику редактирования
                 while True:
-                    ConsoleManager.clear_console()
-                    print("------------------")
+                    clear_console()
                     print_menu_items(menu_single_note)
-                    print("------------------")
 
                     choice3 = input("Choose an item: ")
 
                     if choice3 == "1":
-                        ConsoleManager.clear_console()
-                        command = input("Enter title: ")
-                        print(f"New title: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+                        try:
+                            command = input("Enter title: ")
+                            print(f"New title: {command}")
+                        except ValueError as e:
+                            print(e)
+                        wait_to_continue()
                     elif choice3 == "2":
-                        ConsoleManager.clear_console()
-                        command = input("Enter content: ")
-                        print(f"New content: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+                        try:
+                            command = input("Enter content: ")
+                            print(f"New content: {command}")
+                        except ValueError as e:
+                            print(e)
+                        wait_to_continue()
                     elif choice3 == "3":
-                        ConsoleManager.clear_console()
-                        command = input("Enter tags: ")
-                        print(f"New tags: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+                        try:
+                            command = input("Enter tags: ")
+                            print(f"New tags: {command}")
+                        except ValueError as e:
+                            print(e)
+                        wait_to_continue()
                     elif choice3 == "4":
-                        ConsoleManager.clear_console()
-                        print('Edit whole Note')
-                        print('Example: title;content;#tag1,#tag2')
-                        command = input("Enter info: ")
-                        print(f"You wrote: {command}")
-                        print()
-                        input('Press enter to continue')
+                        clear_console()
+                        try:
+                            print('Edit whole Note')
+                            print('Example: title;content;#tag1,#tag2')
+                            command = input("Enter info: ")
+                            print(f"You wrote: {command}")
+                        except ValueError as e:
+                            print(e)
+                        wait_to_continue()
                     elif choice3 == "5":
-                        ConsoleManager.clear_console()
+                        clear_console()
                         break
                     else:
                         invalid_type()
 
+            # Delete Note
             elif choice2 == "4":
-                ConsoleManager.clear_console()
-                command = input("Delete note: ")
-                print(f"{command} deleted")
-                print()
-                input('Press enter to continue')
+                clear_console()
+
+                try:
+                    command = input("Write Title: ")
+                    noteslist.delete(command)
+                except ValueError as e:
+                    print(e)
+
+                print(f"Note was deleted")
+
+                wait_to_continue()
+
             elif choice2 == "5":
-                ConsoleManager.clear_console()
-                print(f"Notes List")
-                print()
-                input('Press enter to continue')
+                clear_console()
+
+                try:
+                    notes_paginator = Paginator(noteslist)
+                    for page in notes_paginator:
+                        for record in page:
+                            print(record)
+                        input('Press Enter to show the next page...')
+                except ValueError as e:
+                    print(e)
+
+                wait_to_continue()
             elif choice2 == "6":
-                ConsoleManager.clear_console()
+                clear_console()
                 break
             else:
                 invalid_type()
-
+    # Sorting Files
     elif choice1 == "3":
-        ConsoleManager.clear_console()
         while True:
-            ConsoleManager.clear_console()
-            print("------------------")
+            clear_console()
+
+            print("--------------------")
             print("Sort files in folder")
-            print("------------------")
+            print("--------------------")
+            print()
 
             choice2 = input("Folder path (or 'exit'): ")
 
             if choice2 == "exit":
-                ConsoleManager.clear_console()
+                clear_console()
                 break
             else:
-                ConsoleManager.clear_console()
-                sorting = Sorting(choice2)
-                sorting.sort()
-                print('Finish')
-                break
+                clear_console()
 
+                try:
+                    sorting = Sorting(choice2)
+                    sorting.sort()
+                except ValueError as e:
+                    print(e)
+
+                print('Finish')
+                wait_to_continue()
+                break
+    # Close App
     elif choice1 == "4":
-        ConsoleManager.clear_console()
+        clear_console()
         print('See you later!')
         break
     else:
