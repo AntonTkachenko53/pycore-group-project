d9d4ca8d064225ecdb091c8c08a2616866960e85
AntonTkachenko53 <gogochristofour@gmail.com>
2023-10-17 16:05:29 +0300

    AddressBook update, small Pagination fix
    Обновлен АдресБук класс. Переделан под хранение данных в списке, а не в UserDict. Добавлен метод show_all_by_pages со ссылкой на пагинатор.
    В пагинаторе небольшой фикс, так ака дата приходит ему в виде списка.

--- personal_assistant_folder/personal_assistant_folder/AddressBook.py
+++ personal_assistant_folder/personal_assistant_folder/AddressBook.py
@@ -1,14 +1,17 @@
-from collections import UserDict
 from Record import Record
+from Pagination import Paginator
 
 
-class AddressBook(UserDict):
+class AddressBook:
     MIN_LENGTH_ADD = 2
     MAX_LENGTH_ADD = 5
 
+    def __init__(self):
+        self.records = list()
+
     def add_record(self, user_input):
         '''
-        тут введений нпут може містити тільки значення, від двох до п'яти, котрі записуються в record,
+        тут введений інпут може містити тільки значення, від двох до п'яти, котрі записуються в record,
         порядок наступний: name, phone, далі ключові аргументи, котрі за замовчуванням = None: birthday, email, address.
         '''
         commands = user_input.strip().split(' ')
@@ -17,25 +20,36 @@ class AddressBook(UserDict):
 
         name, phone, *args = commands  # Розпакування перших двох значень та всіх інших у змінну args
 
-        for record in self.data.values():
+        for record in self.records:
             if name == record.name._value:
                 raise ValueError('Contact with this name already exists')
 
         try:
-            self.data[name] = Record(name, phone, *args)
+            contact = Record(name, phone, *args)
+            self.records.append(contact)
         except ValueError:
-            pass
+            raise ValueError('Invalid info to create a record')
 
     def find_record(self, searching_str: str):
         result = [
-            record for record in self.data.values() if
+            record for record in self.records if
             searching_str in f"{record.name._value} {record.phone._value} "
                              f"{record.birthday._value} {record.email._value} {record.address._value}"
         ]
         return result
 
     def delete_record(self, record_to_delete: str):
-        try:
-            self.data.pop(record_to_delete)
-        except KeyError:
-            pass
+        for record in self.records:
+            if record_to_delete == record.name._value:
+                self.records.remove(record)
+
+    def show_all_by_pages(self, command, num_of_records=5):
+        if command in ['n', 'p', 'q']:
+            try:
+                paginator = Paginator(self.records, num_of_records)
+                current_page = next(paginator)
+                paginator.move(command)
+            except StopIteration:
+                raise StopIteration('Showing stopped: quit called or end of records')
+        else:
+            raise ValueError('Invalid command.')
