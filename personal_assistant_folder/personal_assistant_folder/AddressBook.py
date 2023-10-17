57cbec8a0736cdf438bf33788dc7f08d35fd40bb
Mykyta Terpelovskyi <81818898+Dremorio@users.noreply.github.com>
2023-10-17 21:39:27 +0300

    Update Serialization.py, AddressBook.py
    Додано функціонал зберігання даних після будь-якої їх зміни

--- personal_assistant_folder/personal_assistant_folder/AddressBook.py
+++ personal_assistant_folder/personal_assistant_folder/AddressBook.py
@@ -1,23 +1,27 @@
 from Record import Record
+from Serialization import Serialization
 
 
 class AddressBook:
     MIN_LENGTH_ADD = 2
     MAX_LENGTH_ADD = 5
+    DEFAULT_FILENAME = 'address_book.bin'
 
     def __init__(self):
-        self.records = list()
+        self.records = Serialization.load_from_file(self.DEFAULT_FILENAME)
 
     def add_record(self, user_input):
-        '''
+        """
         тут введений інпут може містити тільки значення, від двох до п'яти, котрі записуються в record,
         порядок наступний: name, phone, далі ключові аргументи, котрі за замовчуванням = None: birthday, email, address.
-        '''
+        """
         commands = user_input.strip().split(' ')
         if not (self.MIN_LENGTH_ADD <= len(commands) <= self.MAX_LENGTH_ADD):
             raise ValueError('Enter correct info to add a record')
 
         name, phone, *args = commands  # Розпакування перших двох значень та всіх інших у змінну args
+
+        Serialization.save_to_file(self.records, self.DEFAULT_FILENAME)
 
         for record in self.records:
             if name == record.name._value:
@@ -43,3 +47,5 @@ class AddressBook:
         for record in self.records:
             if record_to_delete == record.name._value:
                 self.records.remove(record)
+
+        Serialization.save_to_file(self.records, self.DEFAULT_FILENAME)
