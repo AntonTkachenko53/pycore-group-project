d435ab5adafb00b64b8f5d0f0038fbe1ec6edfa6
Mykyta Terpelovskyi <81818898+Dremorio@users.noreply.github.com>
2023-10-17 23:17:20 +0300

    Update Pagination.py

--- personal_assistant_folder/personal_assistant_folder/Pagination.py
+++ personal_assistant_folder/personal_assistant_folder/Pagination.py
@@ -1,14 +1,17 @@
+import math
+
 class Paginator:
     def __init__(self, data: list, num_of_records=5):
         self.data = data
+        self.total_pages = math.ceil(len(data) / num_of_records)
         self.num_of_records = num_of_records
-        self.current_page = 0
+        self.current_page = 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        start_index = self.current_page * self.num_of_records
+        start_index = (self.current_page - 1) * self.num_of_records
         last_index = start_index + self.num_of_records
         page_data = self.data[start_index:last_index]
 
@@ -17,13 +20,9 @@ class Paginator:
         return page_data
 
     def move(self, direction):
-        if direction == 'n':
-            next_page = (self.current_page + 1) * self.num_of_records
-            if next_page < len(self.data):
-                self.current_page += 1
-            else:
-                print('You are on the last page.')
-        elif direction == 'p' and self.current_page > 0:
+        if direction == 'n' and self.current_page < self.total_pages:
+            self.current_page += 1
+        elif direction == 'p' and self.current_page > 1:
             self.current_page -= 1
         elif direction == 'q':
             raise StopIteration
