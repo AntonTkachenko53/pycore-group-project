11cc96b6e85dc1cce3f138242d11e67ec5ef5bbb
Mykyta Terpelovskyi <81818898+Dremorio@users.noreply.github.com>
2023-10-17 15:11:35 +0300

    Update Serialization.py

--- personal_assistant_folder/personal_assistant_folder/Serialization.py
+++ personal_assistant_folder/personal_assistant_folder/Serialization.py
@@ -1,16 +1,28 @@
 import pickle
 import os
+from shutil import copy
 
 
 class Serialization:
     @staticmethod
     def save_to_file(data, filename):
-        with open(filename, 'wb') as file:
-            pickle.dump(data, file)
+        backup_file = filename + '.backup'
+        if os.path.exists(filename):
+            copy(filename, backup_file)
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(data, file)
+        except Exception:
+            if os.path.exists(backup_file):
+                copy(backup_file, filename)
+            raise
 
     @staticmethod
     def load_from_file(filename):
         if os.path.exists(filename):
-            with open(filename, 'rb') as file:
-                return pickle.load(file)
+            try:
+                with open(filename, 'rb') as file:
+                    return pickle.load(file)
+            except Exception:
+                raise
         return None
